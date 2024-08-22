976777bef84539eb0834b1a6507fed7736f07c90
Memolajuega <111446138+Memolajuega@users.noreply.github.com>
2024-08-22 11:51:02 -0300

    Type1 y Font

--- src/app/components/Opc1-Type1.js
+++ src/app/components/Opc1-Type1.js
@@ -1,14 +1,24 @@
 "use client";
 
 import { useState } from 'react';
-import '../../app/globals.css';
-
+import '../../app/globals.css'; 
 export default function Opc1Type1({ respuesta }) {
-    
+  
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked); 
+  };
+
+  const backgroundColor = isClicked ? '#FC4F4F' : '#4B1572'; 
+
   return (
-    <div className='opc1-type1'>
-      <div className='sombrita'></div>
-      <div className='rta'>{respuesta}</div>
+    <div
+      className='opc1-type1' 
+      style={{ backgroundColor }} 
+      onClick={handleClick}
+    >
+      <div className='rta-type1'>{respuesta}</div>
     </div>
   );
 }
--- src/app/components/Opc2-Type1.js
+++ src/app/components/Opc2-Type1.js
@@ -1,14 +1,24 @@
 "use client";
 
 import { useState } from 'react';
-import '../../app/globals.css';
-
+import '../../app/globals.css'; 
 export default function Opc2Type1({ respuesta }) {
+  
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked); 
+  };
+
+  const backgroundColor = isClicked ? '#4B9C61' : '#4B1572'; 
 
   return (
-    <div className='opc2-type1'>
-      <div className='sombrita'></div>
-      <div className='rta'>{respuesta}</div>
+    <div
+      className='opc2-type1' 
+      style={{ backgroundColor }} 
+      onClick={handleClick}
+    >
+      <div className='rta-type1'>{respuesta}</div>
     </div>
   );
 }
