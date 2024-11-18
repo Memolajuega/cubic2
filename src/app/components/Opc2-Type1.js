7953ba42f12d6787577b8237a7263f0b8a844871
Memolajuega <111446138+Memolajuega@users.noreply.github.com>
2024-11-18 19:12:27 -0300

    Shuffler Nivel2

--- src/app/components/Opc2-Type1.js
+++ src/app/components/Opc2-Type1.js
@@ -1,25 +1,21 @@
-"use client";
+export default function Opc2Type1({ respuesta, isClicked, setIsClicked, isCorrect, seleccionada, onClick }) {
+  const backgroundColor = isClicked
+    ? (isCorrect ? '#4B9C61' : '#FC4F4F') // Verde para correcto, rojo para incorrecto
+    : '#4B1572'; // Fondo inicial
 
-import { useState } from 'react';
-import '../../app/globals.css'; 
-
-export default function Opc2Type1({ respuesta, isClicked, setIsClicked }) {
-  const handleClick = () => {
-    setIsClicked(true);
-  };
-
-  const backgroundColor = isClicked ? '#4B9C61' : '#4B1572';
-  const sombraBackground = isClicked
-  ? 'radial-gradient(ellipse at center, rgba(152, 206, 167, 1) 20%, rgba(152, 206, 167, 0) 100%)'
-  : 'radial-gradient(ellipse at center, rgba(156, 81, 183, 1) 20%, rgba(156, 81, 183, 0) 100%)';
+  /* const sombraBackground = isClicked
+    ? (isCorrect ? 'radial-gradient(ellipse at center, rgba(152, 206, 167, 1) 20%, rgba(152, 206, 167, 0) 100%)' 
+                : 'radial-gradient(ellipse at center, rgba(236, 170, 170, 1) 20%, rgba(236, 170, 170, 0) 100%)')
+    : 'radial-gradient(ellipse at center, rgba(156, 81, 183, 1) 20%, rgba(156, 81, 183, 0) 100%)'; 
+    
+    <div className='sombritatype1' style={{ background: sombraBackground }}></div> */
 
   return (
-    <div
-      className='opc2-type1' 
-      style={{ backgroundColor }} 
-      onClick={handleClick}
-    >
-      <div className='rta-type2'>{respuesta}</div>
+    <div className='opc2-type1'
+      style={{ backgroundColor }}
+      onClick={() => onClick(respuesta)}>
+      
+      <div className='rta-type1'>{respuesta}</div>
     </div>
   );
 }
--- src/app/type1/page.tsx
+++ src/app/type1/page.tsx
@@ -10,27 +10,37 @@ import Indexador from "../components/indexador";
 import Link from 'next/link';
 
 export default function Home() {
-  const [index, setIndex] = useState(0);
-  const [pregunta, setPregunta] = useState('');
-  const [respuesta1, setRespuesta1] = useState('');
-  const [respuesta2, setRespuesta2] = useState('');
-  const [respuesta3, setRespuesta3] = useState('');
-  const [opc1type1Clicked, setOpc1type1Clicked] = useState(false);
-  const [opc2type1Clicked, setOpc2type1Clicked] = useState(false);
-  const [opc3type1Clicked, setOpc3type1Clicked] = useState(false);
+  const [index, setIndex] = useState<number>(0); 
+  const [pregunta, setPregunta] = useState<string>('');
+  const [respuestas, setRespuestas] = useState<string[]>([]);
+  const [opc1type1Clicked, setOpc1type1Clicked] = useState<boolean>(false);
+  const [opc2type1Clicked, setOpc2type1Clicked] = useState<boolean>(false);
+  const [opc3type1Clicked, setOpc3type1Clicked] = useState<boolean>(false);
+  const [respuestaCorrecta, setRespuestaCorrecta] = useState<string>(''); // Respuesta correcta
+  const [seleccionada, setSeleccionada] = useState<string>(''); // Respuesta seleccionada
+  const [aciertos, setAciertos] = useState<number>(0); // Aciertos
+  const [errores, setErrores] = useState<number>(0); // Errores
 
   const supabase = createClient();
-  const totalPreguntas = 4;
+  const totalPreguntas = 5;
 
   const fetchPreguntas = async () => {
     const { data: preguntas } = await supabase.from("textolargo").select("*");
     
     if (preguntas && preguntas.length > index) {
       setPregunta(preguntas[index].Pregunta);
-      setRespuesta1(preguntas[index].Rta1);
-      setRespuesta2(preguntas[index].Rta2);
-      setRespuesta3(preguntas[index].Rta3);
+      const respuestasArray = [
+        preguntas[index].Rta1,
+        preguntas[index].Rta2,
+        preguntas[index].Rta3
+      ];
+      setRespuestaCorrecta(preguntas[index].Rta2); // Obtener la respuesta correcta
+      setRespuestas(shuffleArray(respuestasArray)); // Mezclar las respuestas
     }
+  };
+
+  const shuffleArray = (array: string[]): string[] => {
+    return array.sort(() => Math.random() - 0.5);
   };
 
   useEffect(() => {
@@ -38,6 +48,7 @@ export default function Home() {
     setOpc1type1Clicked(false); 
     setOpc2type1Clicked(false); 
     setOpc3type1Clicked(false);
+    setSeleccionada(''); // Resetear respuesta seleccionada
   }, [index]);
 
   const handleIncrementIndex = () => {
@@ -51,15 +62,33 @@ export default function Home() {
   const progresoAncho = ((index) / totalPreguntas) * 100 + '%';
 
   const handleOpc1Click = () => {
+    setSeleccionada(respuestas[0]);
     setOpc1type1Clicked(true);
+    if (respuestas[0] === respuestaCorrecta) {
+      setAciertos(prevAciertos => prevAciertos + 1);
+    } else {
+      setErrores(prevErrores => prevErrores + 1);
+    }
   };
 
   const handleOpc2Click = () => {
+    setSeleccionada(respuestas[1]);
     setOpc2type1Clicked(true);
+    if (respuestas[1] === respuestaCorrecta) {
+      setAciertos(prevAciertos => prevAciertos + 1);
+    } else {
+      setErrores(prevErrores => prevErrores + 1);
+    }
   };
 
   const handleOpc3Click = () => {
+    setSeleccionada(respuestas[2]);
     setOpc3type1Clicked(true);
+    if (respuestas[2] === respuestaCorrecta) {
+      setAciertos(prevAciertos => prevAciertos + 1);
+    } else {
+      setErrores(prevErrores => prevErrores + 1);
+    }
   };
 
   const handleContinuarClick = () => {
@@ -67,6 +96,7 @@ export default function Home() {
     setOpc1type1Clicked(false); 
     setOpc2type1Clicked(false); 
     setOpc3type1Clicked(false);
+    setSeleccionada('');
   };
 
   return (
@@ -96,15 +126,36 @@ export default function Home() {
         {index < totalPreguntas ? (
           <div className="nivel">
             <div className="pregunta">{pregunta}</div>
-            <Opc1Type1 respuesta={respuesta1} isClicked={opc1type1Clicked} setIsClicked={setOpc1type1Clicked} />
-            <Opc2Type1 respuesta={respuesta2} isClicked={opc2type1Clicked} setIsClicked={setOpc2type1Clicked} />
-            <Opc3Type1 respuesta={respuesta3} isClicked={opc3type1Clicked} setIsClicked={setOpc3type1Clicked} />
+            <Opc1Type1 
+              respuesta={respuestas[0]} 
+              isClicked={opc1type1Clicked} 
+              setIsClicked={setOpc1type1Clicked} 
+              isCorrect={respuestas[0] === respuestaCorrecta} 
+              seleccionada={seleccionada}
+              onClick={handleOpc1Click}
+            />
+            <Opc2Type1 
+              respuesta={respuestas[1]} 
+              isClicked={opc2type1Clicked} 
+              setIsClicked={setOpc2type1Clicked} 
+              isCorrect={respuestas[1] === respuestaCorrecta} 
+              seleccionada={seleccionada}
+              onClick={handleOpc2Click}
+            />
+            <Opc3Type1 
+              respuesta={respuestas[2]} 
+              isClicked={opc3type1Clicked} 
+              setIsClicked={setOpc3type1Clicked} 
+              isCorrect={respuestas[2] === respuestaCorrecta} 
+              seleccionada={seleccionada}
+              onClick={handleOpc3Click}
+            />
           </div>
         ) : (
           <div className="victoria">
             <div className="cuadro">
               <h1>¡Felicitaciones, ganaste!</h1>
-              <h2>Sumaste {index} estrellas</h2>
+              <h2>Sumaste {aciertos} estrellas</h2>
               <img src="/star.png" alt="estrella" className="star"/>
             </div>
           </div>
@@ -115,7 +166,7 @@ export default function Home() {
             onIncrement={handleContinuarClick} 
             onDecrement={handleDecrementIndex} 
             index={index}
-            showContinuar={opc2type1Clicked}
+            showContinuar={opc1type1Clicked || opc2type1Clicked || opc3type1Clicked} 
           />
         )}
       </div>
