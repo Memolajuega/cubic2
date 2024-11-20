01cfb2cf9460b8e7fd31e1ae89c1ae06203677fa
Memolajuega <111446138+Memolajuega@users.noreply.github.com>
2024-11-19 23:52:07 -0300

    Nivel3

--- src/app/type2/page.tsx
+++ src/app/type2/page.tsx
@@ -1,18 +1,19 @@
-"use client"
+"use client";
 
 import { useState, useEffect } from "react";
 import styles from "./page.module.css";
 import { createClient } from "../components/(supabase)/clientClient";
 import Indexador from "../components/indexador";
-import Link from 'next/link';
+import Link from "next/link";
 import Opc2 from "../components/Opc2";
 
 export default function Home() {
   const [index, setIndex] = useState(0);
-  const [pregunta, setPregunta] = useState('');
-  const [respuesta2, setRespuesta2] = useState('');
+  const [pregunta, setPregunta] = useState("");
+  const [respuesta2, setRespuesta2] = useState("");
   const [opc2Clicked, setOpc2Clicked] = useState(false);
-  const [userResponse, setUserResponse] = useState('');
+  const [userResponse, setUserResponse] = useState("");
+  const [buttonColor, setButtonColor] = useState("#4B1572"); // Color inicial del botón
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUserResponse(e.target.value);
@@ -21,7 +22,8 @@ export default function Home() {
   const handleSubmit = () => {
     if (userResponse === respuesta2) {
       setOpc2Clicked(true); // Esto habilita el botón de "Continuar"
-      setUserResponse(''); // Borra el campo de respuesta
+      setButtonColor("#4B9C61"); // Cambia el color del botón al verde
+      setUserResponse(""); // Borra el campo de respuesta
     } else {
       alert("Respuesta incorrecta. Intenta de nuevo.");
     }
@@ -32,7 +34,7 @@ export default function Home() {
 
   const fetchPreguntas = async () => {
     const { data: preguntas } = await supabase.from("escribir").select("*");
-    
+
     if (preguntas && preguntas.length > index) {
       setPregunta(preguntas[index].Pregunta);
       setRespuesta2(preguntas[index].Rta);
@@ -41,22 +43,23 @@ export default function Home() {
 
   useEffect(() => {
     fetchPreguntas();
-    setOpc2Clicked(false); 
+    setOpc2Clicked(false);
+    setButtonColor("#4B1572"); // Restaura el color inicial del botón al cambiar de pregunta
   }, [index]);
 
   const handleIncrementIndex = () => {
-    setIndex(prevIndex => Math.min(prevIndex + 1, totalPreguntas));
+    setIndex((prevIndex) => Math.min(prevIndex + 1, totalPreguntas));
   };
 
   const handleDecrementIndex = () => {
-    setIndex(prevIndex => Math.max(0, prevIndex - 1));
+    setIndex((prevIndex) => Math.max(0, prevIndex - 1));
   };
 
-  const progresoAncho = ((index) / totalPreguntas) * 100 + '%';
+  const progresoAncho = (index / totalPreguntas) * 100 + "%";
 
   const handleContinuarClick = () => {
     handleIncrementIndex();
-    setOpc2Clicked(false); 
+    setOpc2Clicked(false);
   };
 
   return (
@@ -66,13 +69,16 @@ export default function Home() {
           <h1>CUBIC</h1>
           <div>
             <img src="/brain.png" alt="" />
-            <a href="">Aprender</a></div>
+            <a href="">Aprender</a>
+          </div>
           <div>
             <img src="/user.png" alt="" />
-            <a href="">Perfil</a></div>
+            <a href="">Perfil</a>
+          </div>
           <div>
             <img src="/target.png" alt="" />
-            <a href="">Desafíos</a></div>
+            <a href="">Desafíos</a>
+          </div>
         </div>
         <div className="top">
           <Link href="/home">
@@ -86,28 +92,35 @@ export default function Home() {
         {index < totalPreguntas ? (
           <div className="nivel">
             <div className="pregunta">{pregunta}</div>
-            <input 
-              type="text" 
-              value={userResponse} 
-              onChange={handleInputChange} 
-              placeholder="Escribe tu respuesta" 
+            <input
+              type="text"
+              value={userResponse}
+              onChange={handleInputChange}
+              placeholder="Escribe tu respuesta"
+              className="escribir"
             />
-            <button onClick={handleSubmit}>Enviar</button>
+            <button
+              onClick={handleSubmit}
+              className="enviar"
+              style={{ backgroundColor: buttonColor }}
+            >
+              Enviar
+            </button>
           </div>
         ) : (
           <div className="victoria">
             <div className="cuadro">
               <h1>¡Felicitaciones, ganaste!</h1>
               <h2>Sumaste {index} estrellas</h2>
-              <img src="/star.png" alt="estrella" className="star"/>
+              <img src="/star.png" alt="estrella" className="star" />
             </div>
           </div>
         )}
 
         {index < totalPreguntas && (
-          <Indexador 
-            onIncrement={handleContinuarClick} 
-            onDecrement={handleDecrementIndex} 
+          <Indexador
+            onIncrement={handleContinuarClick}
+            onDecrement={handleDecrementIndex}
             index={index}
             showContinuar={opc2Clicked}
           />
